76ac906cbcecc44fbfdb3d105f4b1836de104fa2
Vladimir <veffhz@gmail.com>
2018-07-12 09:14:06 +0300

    Add table, tests.

--- test_orm.py
+++ test_orm.py
@@ -5,24 +5,74 @@ conn = sqlite3.connect('example.db')
 
 
 class User(Base):
-    __tablename__ = 'posts'
+    __tablename__ = 'users'
     id = ('int', 'pk')
     username = ('char(256)', 'required')
 
 
-if __name__ == "__main__":
+class Account(Base):
+    __tablename__ = 'accounts'
+    id = ('int', 'pk')
+    user_id = ('int', 'required')
+    no = ('int', 'required')
+
+
+def user_test(user):
+    all_users = user.select_all()
+    print("users -", [one.username for one in all_users])
+
+    # user1 = user.select_by(id=1)
+    # print("user id 1 -", user1[0].username)
+
+    # user2 = user.select_by(id=2, username='joe')
+    # print("user id 2 and name joe -", user2[0].id)
+
+    user.update_by_id(id=1, username='yyy')
+
+    all_users = user.select_all()
+    print("users -", [one.username for one in all_users])
+
+    user.update_all(username='xxx')
+
+    all_users = user.select_all()
+    print("users -", [one.username for one in all_users])
+
+
+def account_test(account):
+    all_accounts = account.select_all()
+    print("accounts -", [one.no for one in all_accounts])
+
+    account.update_by_id(id=4, no=555)
+
+    all_users = account.select_all()
+    print("accounts -", [one.no for one in all_users])
+
+    account.update_all(no=777)
+
+    all_users = account.select_all()
+    print("accounts -", [one.no for one in all_users])
+
+
+def run_test():
     user = User(connection=conn, id=1, username='doe')
     user.save()
 
     user = User(connection=conn, id=2, username='joe')
     user.save()
 
-    all = user.select_all()
-    print(all)
-    data = user.select_by(id=1)
-    print(data)
-    data = user.select_by(id=2, username='joe')
-    print(data[0].id)
-    user.update_by_id(id=1, username='yyy')
-    user.update_all(username='xxx')
+    user_test(user)
+
+    account = Account(connection=conn, id=4, user_id=1, no=123)
+    account.save()
+
+    account = Account(connection=conn, id=5, user_id=2, no=456)
+    account.save()
+
+    account_test(account)
+
     user.drop_table('posts')
+    user.drop_table('accounts')
+
+
+if __name__ == "__main__":
+    run_test()
